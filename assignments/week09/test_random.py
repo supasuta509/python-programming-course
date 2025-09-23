792df7bbba23921dfef1ed1bffbf6c2297d619de
Supasuta <supasuta.t@ku.th>
2025-09-23 08:20:13 +0000

    ...

--- assignments/week09/test_random.py
+++ assignments/week09/test_random.py
@@ -1,7 +1,19 @@
 import random
-
+ #สุ่มตัวเลขจำนวนเต็ม 
 def test_random():
-    random_number = random.randint(1, 100)
+    #สร้างตัวแปร random_numberตัวแปรคือการสุ่มตัวเลขตั้งแต่ 1-10
+    random_number = random.randint(1, 100)   #ประกาศตัวแปร
     print(random_number)
-    
+    guess_number = int(input("what is your guess number"))
+    if  random_number  == guess_number:
+        print("Congratulation")
+
+    elif random_number  < guess_number:
+        print("Too hight")
+
+    elif  random_number > guess_number:
+        print("Too low")  
+
+#print(random_number)
+     
 test_random()
